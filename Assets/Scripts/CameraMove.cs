4587de88b3c36c5584ef9b0b8629dd25c4186fe2
ehdrms0131 <ehdrms1182@gmail.com>
2021-01-20 21:00:03 +0900

    camera

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -20,10 +20,14 @@ public class CameraMove : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
         //회전 방향 결정
-        Vector3 dir = new Vector3(-mouseY, mouseX, 0);
-        dir = Camera.main.transform.TransformDirection(dir);
-        //Mathf.Clamp
+        Vector3 dir = new Vector3(-mouseY, mouseX , 0);
+        dir.Normalize();
+        //dir = Camera.main.transform.TransformDirection(dir);
         //r=ro+vt
         transform.eulerAngles += dir * rotateSpeed * Time.deltaTime;
+
+        Vector3 rotate = transform.eulerAngles;
+        rotate.x = Mathf.Clamp(rotate.x, -90.0f, 90.0f);//각도를 제한
+        transform.eulerAngles = rotate;
     }
 }
