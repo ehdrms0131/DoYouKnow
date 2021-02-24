9862d8d50e129d5452de38613ff47fdde81a4076
Jpierrot <68889645+Jpierrot@users.noreply.github.com>
2021-02-24 23:03:41 +0900

    2/24

---
+++ Assets/c#/Bosspattern.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bosspattern : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //IEnumerator StateCastCrossAttack()
+    //{
+    //    float randomY = Random.Range(0, 360f);
+       
+    //}
+}
