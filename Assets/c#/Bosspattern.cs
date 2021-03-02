068c5b56f482cc3fd2f2cc00e45939b0f2263309
Jpierrot <s20015@gsm.hs.kr>
2021-03-02 20:02:00 +0900

    Update Bosspattern.cs

--- Assets/c#/Bosspattern.cs
+++ Assets/c#/Bosspattern.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Bosspattern : MonoBehaviour
 {
+
+    [SerializeField] float m_angle = 0f;
+    [SerializeField] float m_distance = 0f;
+    [SerializeField] LayerMask m_layerMask = 0;
+
+    public Object bolt;
     // Start is called before the first frame update
     void Start()
     {
-        
+        bolt = GetComponent<Object>();
     }
 
     // Update is called once per frame
@@ -16,9 +22,43 @@ public class Bosspattern : MonoBehaviour
         
     }
 
+    void BossPatern()
+    {
+        Collider[] t_cols = Physics.OverlapSphere(transform.position, m_distance, m_layerMask);
+
+        if(t_cols.Length > 0)
+        {
+            Debug.Log("플레이어 검출중");
+            Transform t_tfPlayer = t_cols[0].transform;
+
+            Vector3 t_direction = (t_tfPlayer.position - transform.position).normalized;
+            float t_angle = Vector3.Angle(t_direction, transform.forward);
+            if(t_angle < m_angle * 0.5f)
+            {
+                if(Physics.Raycast(transform.position, t_direction, out RaycastHit t_hit, m_distance))
+                {
+                    Debug.Log("볼트 레이케스트");
+                    transform.position = Vector3.Lerp(transform.position, t_hit.transform.position, 2f);
+                }
+            }
+
+
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Rod"))
+        {
+            Instantiate(bolt);
+            Debug.Log("보스 피격");
+        }
+    }
+
     //IEnumerator StateCastCrossAttack()
     //{
     //    float randomY = Random.Range(0, 360f);
-       
+
     //}
 }
