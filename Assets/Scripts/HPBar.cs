6a49732e1e1dd1bdf99fa2ffa5ea6c7b7098ed8d
ehdrms0131 <ehdrms1182@gmail.com>
2021-03-03 21:08:21 +0900

    hpbar

---
+++ Assets/Scripts/HPBar.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPBar : MonoBehaviour
+{
+    [SerializeField]
+    Transform hp;
+    [SerializeField]
+    Camera cam;
+    [SerializeField]
+    Slider slider;
+
+    private void Awake()
+    {
+
+        InvokeRepeating("Find", 0, 1f);
+        InvokeRepeating("Lost", 0, 1f);
+        cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+
+    }
+    private void Update()
+    {
+        Quaternion q_hp = Quaternion.LookRotation(hp.position - cam.transform.position);
+
+        Vector3 hp_angle = Quaternion.RotateTowards(hp.rotation, q_hp, 200).eulerAngles;
+
+        hp.rotation = Quaternion.Euler(0, hp_angle.y, 0);
+
+        //Debug.Log(slider.value);
+        slider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+    }
+
+    // Invoked when the value of the slider changes.
+    public void ValueChangeCheck()
+    {
+        Debug.Log(slider.value);
+    }
+    void Find()
+    {
+        
+    }
+    
+    void Lost()
+    {
+        //slider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+    }
+}
